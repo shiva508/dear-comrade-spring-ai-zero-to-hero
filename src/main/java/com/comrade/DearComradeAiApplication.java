91b0e2e4f34e06620072d00eb45a3e4ff8fa1e4e
shiva508 <dasarishiva1@gail.com>
2024-03-05 10:02:11 +0530

    refactor code

--- src/main/java/com/comrade/DearComradeAiApplication.java
+++ src/main/java/com/comrade/DearComradeAiApplication.java
@@ -1,23 +1,11 @@
 package com.comrade;
 
-import com.comrade.service.PdfOperationService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DearComradeAiApplication {
-    @Autowired
-	private PdfOperationService pdfOperationService;
 	public static void main(String[] args) {
 		SpringApplication.run(DearComradeAiApplication.class, args);
-	}
-	@Bean
-	public ApplicationRunner applicationRunner(){
-		return args -> {
-			pdfOperationService.pdfOperationInit();
-        };
 	}
 }
--- src/main/java/com/comrade/controller/OllamaChatController.java
+++ src/main/java/com/comrade/controller/OllamaChatController.java
@@ -2,6 +2,7 @@ package com.comrade.controller;
 
 import com.comrade.model.QuestionModel;
 import com.comrade.service.OllamaChatService;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,13 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
+@AllArgsConstructor
 public class OllamaChatController {
 
     private final OllamaChatService ollamaChatService;
 
-    public OllamaChatController(OllamaChatService ollamaChatService) {
-        this.ollamaChatService = ollamaChatService;
-    }
     @PostMapping("/askQuestion")
     public Map<String,String> askQuestion(@RequestBody QuestionModel questionModel){
         return Map.of("answer",ollamaChatService.askQuestion(questionModel));
